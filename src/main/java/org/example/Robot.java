0b50fbcb0e873e69607feaa2a68a7671d4f2944d
LizzieDworsky <amydworsky15@gmail.com>
2022-07-01 13:14:48 -0600

    created a simple robot class and instantiated it

--- src/main/java/org/example/Robot.java
+++ src/main/java/org/example/Robot.java
@@ -1,4 +1,22 @@
 package org.example;
 
 public class Robot {
+
+//    Instance variables (HAS A)
+    public String name;
+    public String weaponName;
+    public int weaponPower;
+
+
+//    Constructor
+    public Robot() {
+        this.name = "Iron Giant";
+        this.weaponPower = 50;
+        this.weaponName = "Laser Gun";
+    }
+
+//    Methods (CAN DO)
+    public void Attack(){
+        System.out.println(this.name + " attacks with their " + this.weaponName + " for " + this.weaponPower + " damage!");
+    }
 }
