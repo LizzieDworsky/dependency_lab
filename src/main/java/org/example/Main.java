f854abf525aed07dac91b8f433c79055f4b645b3
LizzieDworsky <amydworsky15@gmail.com>
2022-07-01 14:26:20 -0600

    changed individual weapon instantiations to be declared as their parent class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        LaserVision laserVision = new LaserVision();
-        ArmCannon armCannon = new ArmCannon();
-        EnergyClaw energyClaw = new EnergyClaw();
+        Weapon laserVision = new LaserVision();
+        Weapon armCannon = new ArmCannon();
+        Weapon energyClaw = new EnergyClaw();
         Attacker robotOne = new Robot(armCannon, "Iron Giant");
         Attacker wolfOne = new Werewolf("Lupin", 40);
         robotOne.Attack();
